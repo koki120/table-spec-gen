869683f2524c622ffd3b3b9c34b9a31bab854913
nakstsu <koki20001206@outllok.jp>
2024-01-20 16:09:45 +0000

    add env/empty✅

--- config/config.go
+++ config/config.go
@@ -17,17 +17,38 @@ var (
 )
 
 func init() {
+	if os.Getenv("DB_USER") == "" {
+		log.Fatal("DB_USER is empty")
+	}
+
+	if os.Getenv("DB_PASSWORD") == "" {
+		log.Fatal("DB_PASSWORD is empty")
+	}
+
+	if os.Getenv("DB_HOST") == "" {
+		log.Fatal("DB_HOST is empty")
+	}
+
+	if os.Getenv("DB_PORT") == "" {
+		log.Fatal("DB_PORT is empty")
+	}
+
 	dbName = os.Getenv("DB_NAME")
 	if dbName == "" {
 		log.Fatal("DB_NAME is empty")
 	}
+
 	output_file_format = os.Getenv("OUTPUT_FILE_FORMAT")
-	if output_file_format == "" || (output_file_format != HTML && output_file_format == MARKDOWN) {
+	if output_file_format == "" {
 		output_file_format = MARKDOWN
 	}
+	if output_file_format != HTML && output_file_format != MARKDOWN {
+		log.Fatalf("%s is unavailable", output_file_format)
+	}
+
 	output_file_name = os.Getenv("OUTPUT_FILE_NAME")
 	if output_file_name == "" {
-		output_file_name = "output"
+		output_file_name = ""
 	}
 }
 
